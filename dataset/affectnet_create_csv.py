75fa90b552536826a8e617a472c8b32c9210113a
Yuxuan Shu <48800547+ClaudiaShu@users.noreply.github.com>
2022-10-06 10:30:54 +0100

    Update affectnet_create_csv.py

--- dataset/affectnet_create_csv.py
+++ dataset/affectnet_create_csv.py
@@ -29,10 +29,10 @@ def take_anno(dir_anno, number_id):
 
 if __name__ == '__main__':
 
-    # dir_images = '/data/users/ys221/data/ABAW/origin_faces/'
-    save_dir = 'D:/Self Improvement/Study/MRes IC/!Personal project/ys221/data/AffectNet/csv/'
-    dir_images = 'D:/Self Improvement/Study/MRes IC/!Personal project/ys221/data/AffectNet/val_set/images/'
-    dir_anno = 'D:/Self Improvement/Study/MRes IC/!Personal project/ys221/data/AffectNet/val_set/annotations/'
+
+    save_dir = '' # Path where you save your csv file
+    dir_images = '' # Path to your images
+    dir_anno = '' # Path to your annotations
 
     isFile = os.path.isfile(save_dir)
     if not isFile:
