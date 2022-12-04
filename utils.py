e22c33c78cd54b898bbffcfc20bee0645f74cdfc
Yuxuan Shu <48800547+ClaudiaShu@users.noreply.github.com>
2022-12-04 17:08:29 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -14,8 +14,8 @@ def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
 def save_config_file(model_checkpoints_folder, args):
     if not os.path.exists(model_checkpoints_folder):
         os.makedirs(model_checkpoints_folder)
-        with open(os.path.join(model_checkpoints_folder, 'config.yml'), 'w') as outfile:
-            yaml.dump(args, outfile, default_flow_style=False)
+    with open(os.path.join(model_checkpoints_folder, 'config.yml'), 'w') as outfile:
+        yaml.dump(args, outfile, default_flow_style=False)
 
 
 def accuracy(output, target, topk=(1,)):
